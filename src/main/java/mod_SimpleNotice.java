dafb49c78182cf43f47a49980a2801b55c171497
feildmaster <admin@feildmaster.com>
2012-06-28 21:36:23 -0500

    [BREAKING] Force UTF-8 in messages

--- src/main/java/mod_SimpleNotice.java
+++ src/main/java/mod_SimpleNotice.java
@@ -1,31 +1,23 @@
 import net.minecraft.client.Minecraft;
 
 public class mod_SimpleNotice extends BaseMod {
-    private final String channel = "SimpleNotice";
-
-    @Override
-    public String getVersion() {
-        return "v1.2.5-1.1";
-    }
-
     @Override
     public void load() {
         // Register channel
-        ModLoader.registerPacketChannel(this, channel);
+        ModLoader.registerPacketChannel(this, "SimpleNotice");
         // Listen to ticks
         ModLoader.setInGameHook(this, true, false);
     }
 
     @Override
     public boolean onTickInGame(float tick, Minecraft game) {
-        // !!! This value needs to be updated every update
         NoticeGui.getInstance(game).tick();
         return true;
     }
 
     @Override
-    public void receiveCustomPacket(ee packet) {
-        NoticeGui.get().addNotice(new String(packet.c));
+    public void receiveCustomPacket(ee Packet250) {
+        NoticeGui.get().addNotice(new String(Packet250.c, java.nio.charset.Charset.forName("UTF-8")));
     }
 
     @Override
@@ -33,5 +25,8 @@ public class mod_SimpleNotice extends BaseMod {
         NoticeGui.get().close();
     }
 
-
+    @Override
+    public String getVersion() {
+        return "1.2 (core: 1.1)";
+    }
 }
